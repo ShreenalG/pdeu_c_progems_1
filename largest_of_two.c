be4215de9506fa4995074b528e2caa0286fbc1fd
Shreenal <shreenal2122@gmail.com>
2025-08-12 10:04:10 +0530

    Add files via upload

---
+++ largest_of_two.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+      int x,y;
+      printf("Enter first numbers : \n");
+      scanf("%d",&x);
+
+      printf("Enter second numbers : \n");
+      scanf("%d",&y);
+
+      if(x>y)
+        printf("\n First number is greater which is : %d",x);
+
+      else
+      printf("\n Second number is greater which is : %d",y);
+}
