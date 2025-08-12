a12388943f94efaa8f2b2892a252a20fd5b5dd2d
Shreenal <shreenal2122@gmail.com>
2025-08-12 09:52:23 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+      int x,y;
+      printf("Enter first numbers : \n");
+      scanf("%d",&x);
+
+      printf("Enter second numbers : \n");
+      scanf("%d",&y);
+
+      printf("%d + %d = %d",x,y,x+y);
+}
