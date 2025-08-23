fb8bf894b8ae5461e31fb90a24e5873ab489afee
Shreenal <shreenal2122@gmail.com>
2025-08-23 14:35:57 +0530

    Add files via upload

---
+++ multiply.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+      float x,y;
+      printf("Enter first numbers : \n");
+      scanf("%f",&x);
+
+      printf("Enter second numbers : \n");
+      scanf("%f",&y);
+
+      printf("%.2f x %.2f = %.2f",x,y,x*y);
+}
