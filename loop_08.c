d6cc4053c4132ebd5393f287511b09b7047eb588
Shreenal <shreenal2122@gmail.com>
2025-09-10 21:32:07 +0530

    Update and rename sum_of_n_odd_no.c to loop_08.c

--- loop_08.c
+++ loop_08.c
@@ -4,11 +4,12 @@ void main()
     int i,n,sum=0;
 
 
-    printf("Enter value till you want sum of odd no. : ");
+    printf("Enter value for n : ");
     scanf("%d",&n);
-    for(i=1;i<=n;i=i+2)
+    for(i=1;i<=n;i=i+1)
     {
-        sum=sum+i;
+        sum=sum+(i*2+1);
     }
     printf("sum of first n odd no. = %d",sum);
 }
+
